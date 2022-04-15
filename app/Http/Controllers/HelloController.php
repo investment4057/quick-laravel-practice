12938897eb4c1f048558f21752773727fb8e0e63
investment4057 <investment4057@gmail.com>
2022-04-16 00:15:38 +0900

    3.2 ビューの基本

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,4 +9,12 @@ class HelloController extends Controller
     public function index() {
         return "こんにちは、世界！";
     }
+
+    public function view()
+    {
+        $data = [
+            'msg' => 'こんにちは、世界！！'
+        ];
+        return view('hello.view', $data);
+    }
 }
